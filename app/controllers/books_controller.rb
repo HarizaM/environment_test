4f214030320f157fe800dbb00d470f4fb4b12952
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-12 22:21:50 -0500

    new book changes4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,5 +65,7 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title)
+      params.require(:book).permit(:author)
+      params.require(:book).permit(:price)
     end
 end
