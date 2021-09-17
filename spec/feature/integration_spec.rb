7d3cd6a2fb5ae1ff6a6e51c15653074b694585c1
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-17 15:14:58 -0500

    testing files

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,19 +1,36 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
-require 'date'
 
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    fill_in 'Author', with: 'JK Rowling'
-    fill_in 'Price', with: 5.01
-    fill_in 'Published Date', with: Date.new(2017,2,3)
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'JK Rowling'
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content('JK Rowling')
-    expect(page).to have_content(5.01)
-    expect(page).to have_content(2017-02-03)
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: 5.05
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(5.05)
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Published_date', with: '09/17/2017'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('09/17/2017')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,22 +2,27 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter')
-    described_class.new(author: 'JK Rowling')
-    described_class.new(price: 5.01)
-    described_class.new(published_date: 2017-02-03)
-  end
+  #subject do
+   # described_class.new(title: 'harry potter')
+  #end
 
   it 'is valid with valid attributes' do
+    described_class.new(title: 'harry potter')
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    subject.author = nil
-    subject.price = nil
-    subject.published_date = nil
-    expect(subject).not_to be_valid
+  it 'is valid with valid attributes' do
+    described_class.new(author: 'JK Rowling')
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    described_class.new(price: 5.05)
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    described_class.new(published_date: '09/21/2017')
+    expect(subject).to be_valid
   end
 end
