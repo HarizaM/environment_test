b057101be2afd4f24deeb40331b6b7097276e055
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-19 00:37:20 -0500

    adding to db

--- db/migrate/20210903181323_create_books.rb
+++ db/migrate/20210903181323_create_books.rb
@@ -4,7 +4,6 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :author
       t.float :price
-      t.string :published_date
       t.timestamps
     end
   end
