060794bfa171d9387c2b9cd9e4781f687e0c1f84
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-18 22:45:16 -0500

    add tests and migrations

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,16 +2,23 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter')
-  end
-
   it 'is valid with valid attributes' do
+    described_class.new(title: 'harry potter')
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    expect(subject).not_to be_valid
+  it 'is valid with valid attributes' do
+    described_class.new(author: 'JK Rowling')
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    described_class.new(price: 5.05)
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    described_class.new(published_date: '09/21/2017')
+    expect(subject).to be_valid
   end
 end
