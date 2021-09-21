32d12045f08fcc458bd374118c920869f1a9ff13
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-20 22:18:41 -0500

    edits to routes and all other htmls/views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,9 +64,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
-      params.require(:book).permit(:author)
-      params.require(:book).permit(:price)
-      params.require(:book).permit(:published_date)
+      params.require(:book).permit(:title, :author, :price, :published_date)
     end
 end
