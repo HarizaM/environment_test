61a860eed8a7ba5371960c409de64da98accf404
HarizaM <71986659+HarizaM@users.noreply.github.com>
2021-09-20 21:30:19 -0500

    edits to routes and new.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :books
   root 'books#index'
+  get 'books/index'
+  get 'books/new'
+  post 'books/new'
+  get 'books/show'
+  get 'books/edit'
+  post 'books/edit'
+  match '/books/create', to: 'books#create', via: :post
+  match '/books/', to: 'books#index', via: :all
+
+  match '/books/delete', to: 'books#delete', via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
